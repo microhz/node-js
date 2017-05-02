deba021bcff98b4980e1f5310e9327e54a61921c
micro <pengmi2@126.com>
2017-05-02 16:15:48 +0800

    test

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,6 @@
 var http = require('http')
+var conf = require('../config/api')
+var base_url = conf.java_server.host + ":" + conf.java_server.port
 module.exports = function (app) {
     // get请求
     app.get('/login',function(req,res) {
@@ -7,7 +9,7 @@ module.exports = function (app) {
 
     app.get('/index',function(req,res) {
         // res.render('page/index');
-        http.get("http://www.jiayouxueba.com/website/item/type/3?_=1493712332461",function(request, response) {
+        http.get(base_url + "/website/item/type/3?_=1493712332461",function(request, response) {
             var data = "";
             request.on('data',function(chunk){
                 data += chunk;
