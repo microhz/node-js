33154c80edcba5729a4dcbc7cdf8d0e3e922b7cb
micro <pengmi2@126.com>
2017-05-03 22:18:48 +0800

    add sync-request

--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,17 @@
 var http = require('http')
 var conf = require('../config/api')
 var router_base = require('../utils/router_base');
-
+var request = require('sync-request');
 function _user_router(app) {
     // get请求
-    app.get('/login',function(req,res) {
+    app.get('/login', function (req, res) {
         res.render('login');
     })
 
-    app.get('/index',function(req,res) {
+    app.get('/index', function (req, res) {
         var base_url = router_base.getRemoteUrl();
-        http.get(base_url + "/website/item/type/3?_=1493712332461",function(request, response) {
-            var data = "";
-            request.on('data',function(chunk){
-                data += chunk;
-                var subject_array = JSON.parse(data).data;
-                var select_options_array = [];
-                res.render('index',{subjects : subject_array});
-            });
-        })
+        var ret = request('GET', base_url + "/website/item/type/3?_=1493712332461");
+        console.log(ret.getBody("UTF-8"));
     })
 }
 module.exports = _user_router;
--- utils/http_utils.js
+++ utils/http_utils.js
@@ -2,18 +2,9 @@
 var http = require('http')
 var promise = require('promise')
 var config = require('../config/api')
-// var request = require('request')
+var request = require('sync-request');
 exports.get = function (url, params, req, res, view, opt) {
-    if (opt.headers) {
-
-    }
-    http.get(url,function(request,response) {
-        var data = "";
-        request.on('data',function(chunk) {
-            data += chunk;
-            res.send(data);
-        })
-    })
+    
 }
 
 exports.post = function (url, params, req, res, opt) {
