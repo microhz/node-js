d7e7e128eb067c11225974f422a947733c37b802
micro <pengmi2@126.com>
2017-05-10 13:41:08 +0800

    add async

--- utils/http_utils.js
+++ utils/http_utils.js
@@ -8,7 +8,7 @@ var Promise = require('Promise')
 module.exports.get = function (url, params, headers) {
     var deferred = Q.defer();
     console.log('get url : ' + url + ", params : " + JSON.stringify(params) + ",headers : " + JSON.stringify(headers));
-    var response = request("GET", url, {headers : headers, qs : params});
+    var response = request("GET", url, { headers: headers, qs: params });
     if (response.statusCode != 200) {
         console("response code error");
         return null;
@@ -19,7 +19,7 @@ module.exports.get = function (url, params, headers) {
 }
 
 module.exports.post = function (url, params) {
-    var response = request('POST', url, {json : params});
+    var response = request('POST', url, { json: params });
     if (response.statusCode != 200) {
         console.log("response code error");
         console.log(response.getBody("utf8"))
@@ -28,30 +28,39 @@ module.exports.post = function (url, params) {
     return response.getBody("UTF-8");
 }
 
-
-function get(url, params, headers){
+// ---- 以下为多个请求汇总 ---
+function get(url, params, headers) {
     var deferred = Q.defer();
     console.log('get url : ' + url + ", params : " + JSON.stringify(params) + ",headers : " + JSON.stringify(headers));
-    var response = request("GET", url, {headers : headers, qs : params});
+    var response = request("GET", url, { headers: headers, qs: params });
+
     if (response.statusCode != 200) {
         console("response code error");
         return null;
     }
     var data = response.getBody("UTF-8");
+    console.log(data);
     deferred.resolve(data);
     return deferred.promise;
 }
 
 var url1 = "http://www.baidu.com";
 var url2 = "http://www.tuicool.com/articles/QnIfqq";
+
 function chainExecute() {
-    get(url1).then(function(res) {
+    var a = [];
+    get(url1).then(function (res) {
+        a.push(res);
         console.log(res);
         console.log('first request exeucted..');
         return get(url2)
-    }).then(function(res2){
+    }).then(function (res2) {
         console.log('second request exeucted..');
         console.log(res2);
+        a.push(res2);
+        return a;
+    }).then(function (a) {
+        console.log(a);
     });
     // 同步串行 
 }
@@ -60,9 +69,9 @@ chainExecute();
 
 function sumAsync() {
     // 多个异步数据汇总 result_array为返回结果
-    Q.all([get(url1),get(url2)]).then(function(result_array) {
+    Q.all([get(url1), get(url2)]).then(function (result_array) {
         console.log(result_array)
     })
 }
 
-sumAsync();
+// sumAsync();
